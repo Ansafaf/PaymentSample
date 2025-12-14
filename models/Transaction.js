750467cad87409c836163884a51ddaee6cc3b0d3
MUHAMMED ANSAF <muhammedansaf325@gmail.com>
2025-12-14 21:00:29 +0530

    mongodb online

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,24 +1,24 @@
-// // Stubbed Transaction model - Mongoose disabled
-// // This file provides a mock implementation to avoid runtime errors when Mongoose is commented out.
+// Stubbed Transaction model - Mongoose disabled
+// This file provides a mock implementation to avoid runtime errors when Mongoose is commented out.
 
-// class TransactionMock {
-//     constructor(data) {
-//         Object.assign(this, data);
-//         // Assign default fields if not provided
-//         this.status = this.status || 'pending';
-//         this.createdAt = this.createdAt || new Date().toISOString();
-//     }
-//     async save() {
-//         // In a real DB, this would persist. Here we just return the instance.
-//         return this;
-//     }
-// }
+class TransactionMock {
+    constructor(data) {
+        Object.assign(this, data);
+        // Assign default fields if not provided
+        this.status = this.status || 'pending';
+        this.createdAt = this.createdAt || new Date().toISOString();
+    }
+    async save() {
+        // In a real DB, this would persist. Here we just return the instance.
+        return this;
+    }
+}
 
-// // Static mock methods
-// TransactionMock.findOne = async (query) => null;
-// TransactionMock.findById = async (id) => null;
-// TransactionMock.findOneAndUpdate = async (filter, update) => null;
-// TransactionMock.find = async (query) => [];
-// TransactionMock.create = async (data) => new TransactionMock(data);
+// Static mock methods
+TransactionMock.findOne = async (query) => null;
+TransactionMock.findById = async (id) => null;
+TransactionMock.findOneAndUpdate = async (filter, update) => null;
+TransactionMock.find = async (query) => [];
+TransactionMock.create = async (data) => new TransactionMock(data);
 
-// module.exports = TransactionMock;
+module.exports = TransactionMock;
